3cdbb3e93eb121b4fb34114a4b4eaf46564362b5
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-09-09 17:54:14 -0400

    I lied
    I ported it myself :3

--- src/main/java/net/artienia/rubinated_nether/recipe/ModRecipeTypes.java
+++ src/main/java/net/artienia/rubinated_nether/recipe/ModRecipeTypes.java
@@ -3,15 +3,15 @@ package net.artienia.rubinated_nether.recipe;
 import net.artienia.rubinated_nether.RubinatedNether;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeType;
-import net.neoforged.eventbus.api.IEventBus;
-import net.neoforged.registries.DeferredRegister;
-import net.neoforged.registries.ForgeRegistries;
-import net.neoforged.registries.RegistryObject;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModRecipeTypes {
-    public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(ForgeRegistries.RECIPE_TYPES, RubinatedNether.MOD_ID);
+    public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(BuiltInRegistries.RECIPE_TYPE, RubinatedNether.MOD_ID);
 
-    public static final RegistryObject<RecipeType<FreezingRecipe>> FREEZING = RECIPE_TYPES.register("freezing", () -> RecipeType.simple(new ResourceLocation(RubinatedNether.MOD_ID, "freezing")));
+    public static final DeferredHolder<RecipeType<?>, RecipeType<FreezingRecipe>> FREEZING = RECIPE_TYPES.register("freezing", () -> RecipeType.simple(ResourceLocation.fromNamespaceAndPath(RubinatedNether.MOD_ID, "freezing")));
 
     public static void register(IEventBus eventBus) {
         RECIPE_TYPES.register(eventBus);
